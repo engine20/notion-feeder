faaf993b6ee7831f976ec121203411c449107bc0
engine20 <59338660+engine20@users.noreply.github.com>
2022-04-10 22:53:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
 import getNewFeedItems from './feed';
 import {
+  getExistingPages,
   addFeedItemToNotion,
-  deleteOldUnreadFeedItemsFromNotion,
+  deleteOldUnreadItemsFromNotion,
 } from './notion';
 import htmlToNotionBlocks from './parser';
 
 async function index() {
   const feedItems = await getNewFeedItems();
+  const existingPages = await getExistingPages(feedItems);
 
   for (let i = 0; i < feedItems.length; i++) {
     const item = feedItems[i];
+
+    const existingEntries = existingPages.find(
+      (page) => page.properties.Link.url === item.link
+    );
+
+    const isNewEntry = existingEntries === undefined;
+
     const notionItem = {
       title: item.title,
       link: item.link,
       content: htmlToNotionBlocks(item.content),
+      feed: item.feed,
+      pubDate: item.pubDate,
+      creator: item['dc:creator'],
     };
-    await addFeedItemToNotion(notionItem);
+
+    if (isNewEntry) {
+      await addFeedItemToNotion(notionItem);
+    }
   }
 
-  await deleteOldUnreadFeedItemsFromNotion();
+  await deleteOldUnreadItemsFromNotion();
 }
 
 index();
